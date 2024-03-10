cef2a7da5d8fd5bd3ba7ba9957a5fa9f0da939f6
sam <aquaticcalf@proton.me>
2024-03-10 06:51:27 +0530

    meow

--- user/serializers.py
+++ user/serializers.py
@@ -4,12 +4,12 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'is_expert', 'password')
+        fields = ('id', 'aadhar', 'is_expert', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data['email'],
+            validated_data['aadhar'],
             validated_data['password'],
             is_expert=validated_data.get('is_expert', False)
         )
