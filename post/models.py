a89dd7cc61522f6611648abd83aa5f133adc490f
sam <aquaticcalf@proton.me>
2024-03-10 11:34:17 +0530

    meow

--- post/models.py
+++ post/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from user.models import User
 
+class Label(models.Model):
+    name = models.CharField(max_length=50)
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     body = models.TextField()
+    label = models.ForeignKey(Label, null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
