ab303f4c8c30973ba2955c7cb24d674b2be97753
sam <aquaticcalf@proton.me>
2024-03-10 09:23:49 +0530

    meow

--- user/managers.py
+++ user/managers.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.db import router
 
 class UserManager(BaseUserManager):
-    def create_user(self, aadhaar, password=None, **extra_fields):
-        if not aadhaar:
-            raise ValueError('aadhaar required')
-        user = self.model(aadhaar=aadhaar, **extra_fields)
+    def create_user(self, username, aadhaar, password=None, **extra_fields):
+        user = self.model(username=username,aadhaar=aadhaar, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, aadhaar, password, **extra_fields):
+    def create_superuser(self, username, aadhaar, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -18,4 +17,6 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('superuser must have is_superuser=True.')
 
-        return self.create_user(aadhaar, password, **extra_fields)
+        user = self.create_user(username, aadhaar, password, **extra_fields)
+       
+        return user
--- user/models.py
+++ user/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from .managers import UserManager
 
 class User(AbstractUser):
+    username = models.CharField(max_length=300, unique=True)
     aadhaar = models.CharField(max_length=12, unique=True)
     is_expert = models.BooleanField(default=False)
     objects = UserManager()
 
-    USERNAME_FIELD = 'aadhaar'
     REQUIRED_FIELDS = []
