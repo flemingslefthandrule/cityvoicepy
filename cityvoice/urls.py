24e70e75bd3305e8fcfb206c174c446a39263342
sam <aquaticcalf@proton.me>
2024-03-10 06:41:36 +0530

    meow

--- cityvoice/urls.py
+++ cityvoice/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/register/', RegisterView.as_view(), name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
