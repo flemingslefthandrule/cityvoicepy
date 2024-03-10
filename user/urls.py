1c88e79076ac215046082554bea0037cc7301038
sam <aquaticcalf@proton.me>
2024-03-10 14:57:46 +0530

    meow

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, userinfo
+from .views import RegisterView, LoginView, userinfo, userposts
 from rest_framework_simplejwt.views import (
     TokenVerifyView,
     TokenRefreshView,
