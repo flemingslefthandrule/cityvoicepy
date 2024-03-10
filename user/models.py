6d180e5a9740de67b19522eb59346c3ccc5737ab
sam <aquaticcalf@proton.me>
2024-03-10 06:39:22 +0530

    meow

--- user/models.py
+++ user/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
 class User(AbstractUser):
     is_expert = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
---
+++ user/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'is_expert', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['email'],
+            validated_data['password'],
+            is_expert=validated_data.get('is_expert', False)
+        )
+        return user
