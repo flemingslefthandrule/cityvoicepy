ee94d07614364ce34db03cb3ada8acbaa1e93790
sam <aquaticcalf@proton.me>
2024-03-10 11:06:04 +0530

    meow

--- post/models.py
+++ post/models.py
@@ -3,6 +3,6 @@ from user.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.TextField()
+    title = models.CharField(max_length=100)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
